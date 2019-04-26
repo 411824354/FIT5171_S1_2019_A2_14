5d2a31af48d8e914f893a35291713d25e97e78f0
Alfred Susanto <asus0002@student.monash.edu>
2019-04-26 23:44:32 +1000

    launchserviceprovider and the unit test

--- src/main/java/rockets/model/LaunchServiceProvider.java
+++ src/main/java/rockets/model/LaunchServiceProvider.java
@@ -16,6 +16,10 @@ public class LaunchServiceProvider extends Entity {
 
     private Set<Rocket> rockets;
 
+    public LaunchServiceProvider(){
+        rockets = Sets.newLinkedHashSet();
+    }
+
     public LaunchServiceProvider(String name, int yearFounded, String country) {
         this.name = name;
         this.yearFounded = yearFounded;
@@ -29,6 +33,7 @@ public class LaunchServiceProvider extends Entity {
     }
 
     public int getYearFounded() {
+
         return yearFounded;
     }
 
@@ -44,11 +49,41 @@ public class LaunchServiceProvider extends Entity {
         return rockets;
     }
 
+    public void setName(String name) {
+        if (name == null)
+            throw new IllegalArgumentException("Null input");
+        if (name.trim().isEmpty())
+            throw new IllegalArgumentException("Empty input");
+        this.name = name;
+    }
+
+    public void setYearFounded(int yearFounded) {
+        if (yearFounded <= 0)
+            throw new IllegalArgumentException("Zero or negative input");
+        if (yearFounded <= 1800 || yearFounded >= 2020)
+            throw new IllegalArgumentException("Not in range input");
+        this.yearFounded = yearFounded;
+    }
+
+    public void setCountry(String country) {
+        if (country == null)
+            throw new IllegalArgumentException("Null input");
+        if (country.trim().isEmpty())
+            throw new IllegalArgumentException("Empty input");
+        this.country = country;
+    }
+
     public void setHeadquarters(String headquarters) {
+        if (headquarters == null)
+            throw new IllegalArgumentException("Null input");
+        if (headquarters.trim().isEmpty())
+            throw new IllegalArgumentException("Empty input");
         this.headquarters = headquarters;
     }
 
     public void setRockets(Set<Rocket> rockets) {
+        if (rockets == null)
+            throw new NullPointerException("Null input");
         this.rockets = rockets;
     }
 
---
+++ src/test/java/rockets/model/LaunchServiceProviderUnitTest.java
@@ -0,0 +1,91 @@
+package rockets.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LaunchServiceProviderUnitTest {
+    private LaunchServiceProvider target;
+    private LaunchServiceProvider target2;
+
+    @BeforeEach
+    public void setUp() {
+        target = new LaunchServiceProvider();
+        target2 = new LaunchServiceProvider("ProviderName", 2000, "Australia");
+    }
+
+    @DisplayName("should throw exception when setName given null")
+    @Test
+    public void shouldThrowIllegalArgumentExceptionWhenSetNameGivenNull() {
+        assertThrows(IllegalArgumentException.class, () ->  target.setName(null));
+    }
+
+    @DisplayName("should throw exception when setCountry given null")
+    @Test
+    public void shouldThrowIllegalArgumentExceptionWhenSetCountryGivenNull() {
+        assertThrows(IllegalArgumentException.class, () ->  target.setCountry(null));
+    }
+
+    @DisplayName("should throw exception when setHeadquaters given null")
+    @Test
+    public void shouldThrowIllegalArgumentExceptionWhenSetHeadquatersGivenNull() {
+        assertThrows(IllegalArgumentException.class, () ->  target.setHeadquarters(null));
+    }
+
+    @DisplayName("should throw exception when setRockets given null")
+    @Test
+    public void shouldThrowNullPointerExceptionWhenSetRocketsGivenNull() {
+        assertThrows(NullPointerException.class, () ->  target.setRockets(null));
+    }
+
+    @DisplayName("should throw exception when setYearFounded given negative or zero input")
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 0})
+    public void shouldThrowIllegalArgumentExceptionWhenSetYearFoundedGivenNegativeOrZeroInput(int arg) {
+        assertThrows(IllegalArgumentException.class, ()-> target.setYearFounded(arg));
+    }
+
+    @DisplayName("should throw exception when setYearFounded given out of range input")
+    @ParameterizedTest
+    @ValueSource(ints = {1700, 2500, 150, 1800, 2020})
+    public void shouldThrowIllegalArgumentExceptionWhenSetYearFoundedGivenOutOfRangeInput(int arg) {
+        assertThrows(IllegalArgumentException.class, ()-> target.setYearFounded(arg));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", " ", "  "})
+    public void shouldThrowIllegalArgumentExceptionIfSetNameGivenEmptyInput(String arg) {
+        assertThrows(IllegalArgumentException.class, () -> target.setName(arg));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", " ", "  "})
+    public void shouldThrowIllegalArgumentExceptionIfSetCountryGivenEmptyInput(String arg) {
+        assertThrows(IllegalArgumentException.class, () -> target.setCountry(arg));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", " ", "  "})
+    public void shouldThrowIllegalArgumentExceptionIfSetHeadquatersGivenEmptyInput(String arg) {
+        assertThrows(IllegalArgumentException.class, () -> target.setHeadquarters(arg));
+    }
+
+    @DisplayName("should return true when two launch service provider have same name yearFounded country")
+    @Test
+    public void shouldReturnTrueWhenProvidersHaveSameNameYearFoundedCountry() {
+        LaunchServiceProvider anotherProvider = new LaunchServiceProvider("ProviderName", 2000, "Australia");
+        assertTrue(target2.equals(anotherProvider));
+    }
+
+    @DisplayName("should return false when two launch service provider have different name yearFounded country")
+    @Test
+    public void shouldReturnTrueWhenProvidersHaveDifferentNameYearFoundedCountry() {
+        LaunchServiceProvider anotherProvider = new LaunchServiceProvider("NameProvider", 1995, "Singapore");
+        assertFalse(target2.equals(anotherProvider));
+    }
+
+}
