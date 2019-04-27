81e2d6528fa6283983c3e21991932bbb2484c0e6
JFourhw <zwan550@student.monash.edu>
2019-04-27 22:22:18 +1000

    mining simplify 1

--- src/test/java/rockets/mining/RocketMinerUnitTest.java
+++ src/test/java/rockets/mining/RocketMinerUnitTest.java
@@ -37,18 +37,19 @@ public class RocketMinerUnitTest {
         dao = mock(Neo4jDAO.class);
         miner = new RocketMiner(dao);
         rockets = Lists.newArrayList();
-
         lsps = Arrays.asList(
                 new LaunchServiceProvider("ULA", 1990, "USA"),
                 new LaunchServiceProvider("SpaceX", 2002, "USA"),
                 new LaunchServiceProvider("ESA", 1975, "Europe ")
         );
-
+        List<Launch> launchList1 = Lists.newArrayList();
         // index of lsp of each rocket
         int[] lspIndex = new int[]{0, 0, 0, 1, 1};
+        int[] countryIndex = new int[]{0,0,0,1,1};
+        String[] countries = new String[]{"CHINA","USA"};
         // 5 rockets
         for (int i = 0; i < 5; i++) {
-            rockets.add(new Rocket("rocket_" + i, "USA", lsps.get(lspIndex[i])));
+            rockets.add(new Rocket("rocket_" + i, countries[countryIndex[i]], lsps.get(lspIndex[i])));
         }
         // month of each launch
         int[] months = new int[]{1, 6, 4, 3, 4, 11, 6, 5, 12, 5};
@@ -57,7 +58,7 @@ public class RocketMinerUnitTest {
         int[] rocketIndex = new int[]{0, 0, 0, 0, 1, 1, 1, 2, 2, 3};
 
         // 10 launches
-        launches = IntStream.range(0, 10).mapToObj(i -> {
+        launchList1 = IntStream.range(0, 10).mapToObj(i -> {
             logger.info("create " + i + " launch in month: " + months[i]);
             Launch l = new Launch();
             l.setLaunchDate(LocalDate.of(2017, months[i], 1));
@@ -67,6 +68,41 @@ public class RocketMinerUnitTest {
             spy(l);
             return l;
         }).collect(Collectors.toList());
+
+        int[] setSize = new int[]{3,3,3,3,2,2,2,1,1,1};
+        String[] type = new String[]{"satellite","spaceStation","spaceProbe"};
+        List<Launch> launchList = new ArrayList<>();
+        int[] outComes = new int[]{1,1,1,0,1,1,0,1,0,0};
+        int[] lspIndex1 = new int[]{0,0,0,0,1,1,1,2,2,2};
+        int[] price = new int[]{4000,5000,6000,1000,1500,8000,3000,9000,10000,2000};
+        // 5 rockets
+        for (int k = 0; k < launchList1.size(); k++) {
+            List<Set<Payload>> setList = new ArrayList<>();
+            Set<Payload> payloads = new HashSet<>();
+            Launch.LaunchOutcome outCome = null;
+            int j = setSize[k];
+            for (int i = 0; i < j; i++) {
+                Payload payload = new Payload("Payload" + j + "" + i, type[j-1]);
+                payloads.add(payload);
+
+            }
+            setList.add(payloads);
+
+            Launch launch = launchList1.get(k);
+            launch.setPayload(payloads);
+            launch.setLaunchVehicle(rockets.get(rocketIndex[k]));
+            if (outComes[k] == 1)
+                outCome = Launch.LaunchOutcome.SUCCESSFUL;
+            else
+                outCome = Launch.LaunchOutcome.FAILED;
+            launch.setLaunchOutcome(outCome);
+            launch.setLaunchServiceProvider(lsps.get(lspIndex1[k]));
+            BigDecimal value = new BigDecimal(price[k]);
+            launch.setPrice(value);
+            launchList.add(launch);
+        }
+        int i = 0;
+        launches = launchList;
     }
 
 
@@ -94,23 +130,7 @@ public class RocketMinerUnitTest {
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 3})
     public void shouldReturnMostReliableLaunchServiceProviders(int k){
-        List<Launch> launches1 = Lists.newArrayList();
-
-        int[] outComes = new int[]{1,1,1,0,1,1,0,1,0,0};
-        int[] lspIndex = new int[]{0,0,0,0,1,1,1,2,2,2};
-        for (int i = 0;i < launches.size();i ++)
-        {
-            Launch.LaunchOutcome outCome = null;
-            if (outComes[i] == 1)
-                outCome = Launch.LaunchOutcome.SUCCESSFUL;
-            else
-                outCome = Launch.LaunchOutcome.FAILED;
-            Launch launch = launches.get(i);
-            launch.setLaunchOutcome(outCome);
-            launch.setLaunchServiceProvider(lsps.get(lspIndex[i]));
-            launches1.add(launch);
-        }
-        when(dao.loadAll(Launch.class)).thenReturn(launches1);
+        when(dao.loadAll(Launch.class)).thenReturn(launches);
         List<LaunchServiceProvider> launchServiceProviders1 = new ArrayList<>(lsps);
         List<LaunchServiceProvider> launchServiceProviders = miner.mostReliableLaunchServiceProviders(k);
         assertEquals(k,launchServiceProviders.size());
@@ -121,18 +141,8 @@ public class RocketMinerUnitTest {
     @ValueSource(ints = {1,2,3})
     public void shouldReturnMostExpensiveLaunches(int k)
     {
-        int[] price = new int[]{4000,5000,6000,1000,1500,8000,3000,9000,10000,2000};
-        List<Launch> launches1 = new ArrayList<>();
-        for (int i = 0; i < launches.size();i++)
-        {
-
-            BigDecimal value = new BigDecimal(price[i]);
-            Launch launch = launches.get(i);
-            launch.setPrice(value);
-            launches1.add(launch);
-        }
-        when(dao.loadAll(Launch.class)).thenReturn(launches1);
-        List<Launch> sortedLaunches = new ArrayList<>(launches1);
+        when(dao.loadAll(Launch.class)).thenReturn(launches);
+        List<Launch> sortedLaunches = new ArrayList<>(launches);
         sortedLaunches.sort((a, b) -> -a.getPrice().compareTo(b.getPrice()));
         List<Launch> launchList = miner.mostExpensiveLaunches(k);
         assertEquals(k,launchList.size());
@@ -160,35 +170,7 @@ public class RocketMinerUnitTest {
     @Test
     public void ShouldReturnDominantCountry()
     {
-        int[] setSize = new int[]{3,3,3,3,2,2,2,1,1,1};
-        String[] type = new String[]{"satellite","spaceStation","spaceProbe"};
-        List<Launch> launchList = new ArrayList<>();
-        List<Rocket> rocketList = Lists.newArrayList();
-        int[] lspIndex = new int[]{0, 0, 0, 1, 1};
-        int[] countryIndex = new int[]{0,0,0,1,1};
-        int[] rocketIndex = new int[]{0, 0, 0, 0, 1, 1, 1, 2, 2, 3};
-        String[] countries = new String[]{"CHINA","USA"};
-        // 5 rockets
-        for (int i = 0; i < 5; i++) {
-            rocketList.add(new Rocket("rocket_" + i, countries[countryIndex[i]], lsps.get(lspIndex[i])));
-        }
-        for (int k = 0; k < launches.size(); k++) {
-            List<Set<Payload>> setList = new ArrayList<>();
-            Set<Payload> payloads = new HashSet<>();
-            int j = setSize[k];
-            for (int i = 0; i < j; i++) {
-                Payload payload = new Payload("Payload" + j + "" + i, type[j-1]);
-                payloads.add(payload);
-
-            }
-            setList.add(payloads);
-
-            Launch launch = launches.get(k);
-            launch.setPayload(payloads);
-            launch.setLaunchVehicle(rocketList.get(rocketIndex[k]));
-            launchList.add(launch);
-        }
-        when(dao.loadAll(Launch.class)).thenReturn(launchList);
+        when(dao.loadAll(Launch.class)).thenReturn(launches);
         String realCountry = "CHINA";
         String testCountry = miner.dominantCountry("LEO");
         assertEquals(realCountry,testCountry);
